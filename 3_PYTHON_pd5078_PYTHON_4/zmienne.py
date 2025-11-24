19b50bccdaaf953a878ac16fc6afcb59d2cd8523
Aleksandra Pascaru <pd5078@pjwstk.edu.pl>
2025-11-24 16:19:11 +0100

    Dodano zadanie PYTHON_4

---
+++ 3_PYTHON_pd5078_PYTHON_4/zmienne.py
@@ -0,0 +1,11 @@
+# zdefiniowanie zmiennych
+
+slowo = "herbata"
+liczba_calkowita = 7
+liczba_ulamkowa = 21.58
+
+# informacje o zmiennych
+
+print("Zmienne występujące w zadaniu to:", slowo, liczba_calkowita, liczba_ulamkowa)
+print("Wybrane słowo to " + slowo + ". Wybrana liczba całkowita to " + str(liczba_calkowita) + ". Wybrany ułamek dziesiętny to " + str(liczba_ulamkowa) + ".")
+print("Typ zmiennej slowo to " + str(type(slowo)) + ". Typ zmiennej liczba_calkowita to " + str(type(liczba_calkowita)) + ". Typ zmiennej liczba_ulamkowa to " + str(type(liczba_ulamkowa)) + ".")
